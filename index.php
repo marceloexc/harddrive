f273214cd44000ce8abdabde679c61ddd1496092
Marcelo Mendez <marceloexc@tutanota.de>
2025-10-30 01:06:36 -0500

    yay better footer

--- index.php
+++ index.php
@@ -95,41 +95,82 @@
 		</main>
 
 		<footer>
-			<img src="/static/soriginal.jpg" >
-			<!-- <p>
-				 such a funny life I lead
-				 </p> -->
-
 			<?php
+			$footer_array = array("/static/soriginal.jpg", "/static/lunch.jpeg", "/static/dusk.jpeg", "/static/still.gif");
 
-			function formatBytes($bytes, $precision = 2) {
-				$units = ['B', 'KB', 'mb', 'GB', 'TB'];
+			$footer_image_key = array_rand($footer_array, 1);
 
-				$bytes = max($bytes, 0);
-				$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-				$pow = min($pow, count($units) - 1);
-
-				$bytes /= pow(1024, $pow);
-				// this will also work in place of the above line:
-				// $bytes /= (1 << (10 * $pow));
-
-				return round($bytes, $precision) . $units[$pow];
-			}
+			$footer_img = $footer_array[$footer_image_key];
+			
+			echo "<img src=$footer_img >"
 
 
-			$total_size = 0;
-			$di = new RecursiveDirectoryIterator('.');
-			foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
-				if($file->isFile()) {
-					/* echo $filename . ' - ' . $file->getSize() . ' bytes <br/>'; */
-					$total_size += $file->getSize();
-				}
-			}
-
-			$total_size_formatted = formatBytes($total_size);
-
-			echo "serving $total_size_formatted from debian linux"; //in bytes
 			?>
+
+			<p>
+				you might like...
+			</p>
+
+			<a href="https://rm2000.app">rm2000 tape recorder for macintosh</a>
+			<br>
+			<a href="https://wiki.xxiivv.com">devine lu linvega</a>
+			<br>
+			<a href="https://valvearchive.com">valve archive</a>
+			<br>
+			<a href="https://stanleylieber.com/">stanley lieber</a>
+			<br>
+			<a href="https://www.epiclylaterd.com/">epicly later'd</a>
+			<br>
+			<a href="https://howcouldyoudothisto.us/">how could you do this to us</a>
+			<br>
+			<a href="https://otto-b.info/" >otto benson</a>
+			<br>
+			<a href="https://www.johnnyhardstaff.com/home/future-of-gaming">hardstaff at his best</a>
+			<br>
+			<a href="https://orllewin.uk/pcr/">pudsey clough radio</a>
+			<br>
+			<a href="https://emacsformacos.com/">emacs for mac os</a>
+			<br>
+			<a href="http://www.danamania.com/print/">cool nerdy posters</a>
+			<br>
+			
+				<?php
+
+				function formatBytes($bytes, $precision = 2) {
+					$units = ['B', 'KB', 'mb', 'GB', 'TB'];
+
+					$bytes = max($bytes, 0);
+					$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+					$pow = min($pow, count($units) - 1);
+
+					$bytes /= pow(1024, $pow);
+					// this will also work in place of the above line:
+					// $bytes /= (1 << (10 * $pow));
+
+					return round($bytes, $precision) . $units[$pow];
+				}
+
+
+				$total_size = 0;
+				$di = new RecursiveDirectoryIterator('.');
+				foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
+					if($file->isFile()) {
+						/* echo $filename . ' - ' . $file->getSize() . ' bytes <br/>'; */
+						$total_size += $file->getSize();
+					}
+				}
+
+				$total_size_formatted = formatBytes($total_size);
+
+				echo "<p>hosted on debian</p>"; //in bytes
+				?>
+
+			<br>	<br>
+			<p>
+				marcelo mendez 2025
+			</p>
+
+			
 		</footer>
 
 	</body>
